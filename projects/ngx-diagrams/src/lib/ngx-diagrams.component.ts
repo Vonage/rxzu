43ddfeb7431248ddb86e3ea5716023cb72b055d2
DanielNetzer <daniel.netzer7@gmail.com>
2019-04-28 10:24:16 +0300

    initial commit

---
+++ projects/ngx-diagrams/src/lib/ngx-diagrams.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-ngx-diagrams',
+  template: `
+    <p>
+      ngx-diagrams works!
+    </p>
+  `,
+  styles: []
+})
+export class NgxDiagramsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
