d073dff855a14c138e1fece6c16785192ad22285
DanielNetzer <daniel.netzer7@gmail.com>
2019-05-06 16:02:09 +0300

    docs(images, stories): fixed storybook imports, favicon, logo

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,14 +1,22 @@
-import { configure, addParameters } from "@storybook/angular";
+import { configure, addParameters } from '@storybook/angular';
+import { create } from '@storybook/theming';
+
+const theme = create({
+  base: 'light',
+	brandTitle: 'NGX-Diagrams',
+  brandImage: '/icon.png'
+});
 
 addParameters({
-  options: {
-    panelPosition: "right"
-  }
+	options: {
+    showPanel: false,
+    theme
+	}
 });
 
 function loadStories() {
-  require("../stories/index.stories.js");
-  // You can require as many stories as you need.
+	require('../stories/index.stories.js');
+	// You can require as many stories as you need.
 }
 
 configure(loadStories, module);
--- stories/index.stories.js
+++ stories/index.stories.js
@@ -1,35 +1,35 @@
-import { storiesOf, moduleMetadata } from "@storybook/angular";
-import { AppComponent } from "projects/playground/src/app/app.component";
-import { MarkdownWrapperComponent } from "projects/playground/src/app/components/markdown-wrapper/markdown-wrapper.component";
-import { NgxDiagramsModule, DefaultNodeComponent } from "ngx-diagrams";
-import { MarkdownModule } from "ngx-markdown";
-import readme from "../README.md";
+import { storiesOf, moduleMetadata } from '@storybook/angular';
+import { AppComponent } from 'projects/playground/src/app/app.component';
+import { MarkdownWrapperComponent } from 'projects/playground/src/app/components/markdown-wrapper/markdown-wrapper.component';
+import { NgxDiagramsModule, DefaultNodeComponent, DefaultPortComponent } from 'ngx-diagrams';
+import { MarkdownModule } from 'ngx-markdown';
+import readme from '../README.md';
 
-storiesOf("Docs", module)
-  .addDecorator(
-    moduleMetadata({
-      imports: [MarkdownModule.forRoot()]
-    })
-  )
-  .add("welcome", () => ({
-    component: MarkdownWrapperComponent,
-    props: {
-      rawMarkdown: readme
-    }
-  }));
+storiesOf('Docs', module)
+	.addDecorator(
+		moduleMetadata({
+			imports: [MarkdownModule.forRoot()]
+		})
+	)
+	.add('welcome', () => ({
+		component: MarkdownWrapperComponent,
+		props: {
+			rawMarkdown: readme
+		}
+	}));
 
-storiesOf("Examples", module)
-  .addDecorator(
-    moduleMetadata({
-      imports: [NgxDiagramsModule],
-      entryComponents: [DefaultNodeComponent]
-    })
-  )
-  .add(
-    "base",
-    () => ({
-      component: AppComponent
-    })
-    // ,
-    // { notes: { markdown: readme } }
-  );
+storiesOf('Examples', module)
+	.addDecorator(
+		moduleMetadata({
+			imports: [NgxDiagramsModule],
+			entryComponents: [DefaultNodeComponent, DefaultPortComponent]
+		})
+	)
+	.add(
+		'simple',
+		() => ({
+			component: AppComponent
+		})
+		// ,
+		// { notes: { markdown: readme } }
+	);
