9861bda25a1c4e4c5c4148bf34ba7b180bd406b4
DanielNetzer <daniel.netzer7@gmail.com>
2019-06-23 16:04:36 +0300

    fix(public api): fixed exposed public apis

--- projects/ngx-diagrams/src/public-api.ts
+++ projects/ngx-diagrams/src/public-api.ts
@@ -9,3 +9,6 @@ export * from './lib/defaults/components/default-node/default-node.component';
 export * from './lib/defaults/components/default-port/default-port.component';
 export * from './lib/defaults/components/default-link/default-link.component';
 export * from './lib/models/diagram.model';
+export * from './lib/defaults/models/default-link.model';
+export * from './lib/defaults/models/default-port.model';
+export * from './lib/defaults/models/default-node.model';
