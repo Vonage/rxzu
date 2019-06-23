69a80de771833cc3d875d3e2f8d78ea1b90441a3
DanielNetzer <daniel.netzer7@gmail.com>
2019-06-23 14:35:24 +0300

    fix(public api): exporting all required modules

--- projects/ngx-diagrams/src/public-api.ts
+++ projects/ngx-diagrams/src/public-api.ts
@@ -7,3 +7,5 @@ export * from './lib/services/engine.service';
 export * from './lib/ngx-diagrams.module';
 export * from './lib/defaults/components/default-node/default-node.component';
 export * from './lib/defaults/components/default-port/default-port.component';
+export * from './lib/defaults/components/default-link/default-link.component';
+export * from './lib/models/diagram.model';
