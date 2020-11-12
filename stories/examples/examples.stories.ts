6e23e2fe3548d8ebc3e4ab9e7add3ebcfd245e84
DanielNetzer <daniel.netzer@vonage.com>
2020-11-12 09:13:42 +0200

    docs(auto arrange): added example story for auto arrange

--- stories/examples/examples.stories.ts
+++ stories/examples/examples.stories.ts
@@ -1,9 +1,17 @@
 import { Meta, moduleMetadata } from '@storybook/angular';
 import { CommonModule } from '@angular/common';
-import { DefaultLabelComponent, DefaultLinkComponent, DefaultNodeComponent, DefaultPortComponent, NgxDiagramsModule } from 'ngx-diagrams';
-import { BasicDiagramTemplate, SerializationDiagramTemplate } from './examples.template';
+import {
+	DagreEngine,
+	DefaultLabelComponent,
+	DefaultLinkComponent,
+	DefaultNodeComponent,
+	DefaultPortComponent,
+	NgxDiagramsModule,
+} from 'ngx-diagrams';
+import { AutoArrangeDiagramTemplate, BasicDiagramTemplate, SerializationDiagramTemplate } from './examples.template';
 import { BasicExampleStoryComponent } from './basic/basic.component';
 import { SerializationExampleStoryComponent } from './serialization/serialization.component';
+import { AutoArrangeExampleStoryComponent } from './auto-arrange/auto-arrange.component';
 
 export default {
 	title: 'Examples',
@@ -15,10 +23,12 @@ export default {
 				DefaultNodeComponent,
 				BasicExampleStoryComponent,
 				SerializationExampleStoryComponent,
+				AutoArrangeExampleStoryComponent,
 				DefaultLinkComponent,
 				DefaultLabelComponent,
 				DefaultPortComponent,
 			],
+			providers: [DagreEngine],
 			imports: [CommonModule, NgxDiagramsModule],
 			entryComponents: [DefaultNodeComponent, DefaultLinkComponent, DefaultLabelComponent, DefaultPortComponent],
 		}),
@@ -28,3 +38,5 @@ export default {
 export const Basic = BasicDiagramTemplate.bind({});
 
 export const Serialization = SerializationDiagramTemplate.bind({});
+
+export const AutoArrange = AutoArrangeDiagramTemplate.bind({});
