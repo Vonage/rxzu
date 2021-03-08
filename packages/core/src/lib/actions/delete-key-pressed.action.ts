311989bee7751bf29dea43d5a4a2718b51c2cf1d
yrabinovitch <yael.rabinovitch@vonage.com>
2021-03-08 09:52:53 +0200

    fix(keyboard): fire delete action only if pressed the delete key

--- packages/core/src/lib/actions/delete-key-pressed.action.ts
+++ packages/core/src/lib/actions/delete-key-pressed.action.ts
@@ -9,31 +9,14 @@ export class DeleteKeyPressedAction extends BaseAction {
     super();
 
     this.deletedModels = [];
-    const options = diagramEngine.getDiagramModel().getKeyBindings().delete;
-    const keyCodes = options?.keyCodes || [46, 8];
-    const modifiers = {
-      ctrlKey: false,
-      shiftKey: false,
-      altKey: false,
-      metaKey: false,
-      ...options?.modifiers
-    };
+    const isNotLocked = (
+      item: NodeModel | PointModel | PortModel | LinkModel
+    ): item is NodeModel | PointModel | PortModel =>
+      !diagramEngine.isModelLocked(item);
 
-    if (keyCodes.indexOf(event.keyCode) !== -1 &&
-      (event.ctrlKey === modifiers.ctrlKey) &&
-      (event.shiftKey === modifiers.shiftKey) &&
-      (event.altKey === modifiers.altKey) &&
-      (event.metaKey === modifiers.metaKey)) {
-
-      const isNotLocked = (
-        item: NodeModel | PointModel | PortModel | LinkModel
-      ): item is NodeModel | PointModel | PortModel =>
-        !diagramEngine.isModelLocked(item);
-
-      this.deletedModels = diagramEngine
-        .getDiagramModel()
-        .getSelectedItems()
-        .filter(isNotLocked);
-    }
+    this.deletedModels = diagramEngine
+      .getDiagramModel()
+      .getSelectedItems()
+      .filter(isNotLocked);
   }
 }
--- packages/core/src/lib/managers/keyboard.manager.ts
+++ packages/core/src/lib/managers/keyboard.manager.ts
@@ -15,10 +15,30 @@ export class KeyboardManager {
   }
 
   onKeyUp(event: KeyboardEvent) {
+    this.handleDeleteKeyPressed(event);
+  }
+
+  handleDeleteKeyPressed(event: KeyboardEvent) {
+    const options = this.engine.getDiagramModel().getKeyBindings().delete;
+    const keyCodes = options?.keyCodes || [46, 8];
+    const modifiers = {
+      ctrlKey: false,
+      shiftKey: false,
+      altKey: false,
+      metaKey: false,
+      ...options?.modifiers
+    };
+
+    if (keyCodes.indexOf(event.keyCode) !== -1 &&
+      (event.ctrlKey === modifiers.ctrlKey) &&
+      (event.shiftKey === modifiers.shiftKey) &&
+      (event.altKey === modifiers.altKey) &&
+      (event.metaKey === modifiers.metaKey)) {
       const deleteKeyPressedAction = new DeleteKeyPressedAction(this.engine, event);
       this.engine.startFiringAction(deleteKeyPressedAction);
       this.engine.fireAction();
       this.engine.stopFiringAction();
+    }
   }
 
   onCopy() {
