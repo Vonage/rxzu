aef0b3fbd9f8a574afff95bf4c28090778a094c8
DanielNetzer <daniel.netzer@vonage.com>
2021-04-03 15:45:51 +0300

    docs(angular demo): updated demo application

--- apps/angular-demo/src/app/app.module.ts
+++ apps/angular-demo/src/app/app.module.ts
@@ -1,13 +1,39 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RxZuDefaultsModule } from '@rxzu/angular';
+import {
+  ComponentProviderOptions,
+  DefaultLabelComponent,
+  DefaultLinkComponent,
+  DefaultNodeComponent,
+  DefaultPortComponent,
+  RxZuModule,
+} from '@rxzu/angular';
 
 import { AppComponent } from './app.component';
 
+const DEFAULTS: ComponentProviderOptions[] = [
+  {
+    type: 'node',
+    component: DefaultNodeComponent,
+  },
+  {
+    type: 'port',
+    component: DefaultPortComponent,
+  },
+  {
+    type: 'link',
+    component: DefaultLinkComponent,
+  },
+  {
+    type: 'label',
+    component: DefaultLabelComponent,
+  },
+];
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, CommonModule, RxZuDefaultsModule],
+  imports: [BrowserModule, CommonModule, RxZuModule.withComponents(DEFAULTS)],
   providers: [],
   bootstrap: [AppComponent],
 })
